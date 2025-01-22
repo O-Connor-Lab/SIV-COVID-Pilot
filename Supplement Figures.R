77d5680d8b7229f1bfeacb727e8e986612f2d438
Zo Kolodner <161759381+zokolo@users.noreply.github.com>
2025-01-22 15:42:06 -0800

    Update Supplement Figures.R

--- Supplement Figures.R
+++ Supplement Figures.R
@@ -1,3 +1,4 @@
+#Load in libraries
 library(tidyverse)
 library(reshape2)
 library(ggnewscale)
@@ -16,19 +17,11 @@ FeatureTable <- read_qza("Data/featuretable.qza")$data #Feature Table
 metadata<-read.table("Data/Sample-Metadata-Numeric.txt", header = TRUE) #Read in metadata
 colnames(metadata) <-  c('SampleID', 'patient', 'location', 'time') #make sure the column names are readable
 metadata$location[metadata$location == "NasalSwab"] <- "Nasal Swab" #Adjust the location labels
-metadata$location[metadata$location == "RectalSwab"] <- "Rectal Swab"
-metadata$location[metadata$location == "ThroatSwab"] <- "Tracheal Swab"
+metadata$location[metadata$location == "RectalSwab"] <- "Rectal Swab" #Adjust the location labels
+metadata$location[metadata$location == "ThroatSwab"] <- "Tracheal Swab" #Adjust the location labels
 metadata$time <- as.factor(metadata$time) #Turns numeric values into categorical values
-metadata$location <- fct_relevel(metadata$location, "Nasal Swab", "Tracheal Swab", "Rectal Swab", "Stool")
-metadata$time <- fct_relevel(metadata$time, "-7", "3", "5", "7", "10", "14")
-
-
-#Reads in shannon diversity from QIIME2
-shannon <- read_qza("Outputs/Diversity/rarefied-shannon.qza")$data %>%
-  rownames_to_column(var='SampleID') %>%
-  inner_join(metadata) %>%
-  filter(location != 'control')
-write.csv(shannon,"Figures/Supplemental/Shannon-Diversity.csv")
+metadata$location <- fct_relevel(metadata$location, "Nasal Swab", "Tracheal Swab", "Rectal Swab", "Stool") #Refactor locations
+metadata$time <- fct_relevel(metadata$time, "-7", "3", "5", "7", "10", "14") #Refactor time into chronological order
 
 #Read in taxonomy
 taxonomy<-read_qza("Data/taxonomy_2.qza") #read in taxonomy
@@ -39,6 +32,7 @@ taxonomy<-parse_taxonomy(taxonomy$data) #Removes the confidence from the taxonom
 taxasums <- summarize_taxa(FeatureTable, taxonomy)$Genus
 write.csv(taxasums, "Figures/Supplemental/Feature_Table.csv", row.names = TRUE)
 
+#Keeps colors consistant across graphs
 col <- c("f_Lachnospiraceae" = '#DDD8EF',
          "f_Rhodocyclaceae" = '#D1C1E1', 
          "f_Ruminococcaceae" = '#C3A8D1',
@@ -87,6 +81,7 @@ col <- c("f_Lachnospiraceae" = '#DDD8EF',
          "g_Sphingomonas" = "#729C33",
          "Other" = "#777777")
 
+#Keeps theme constant across graphs
 theme_custom <- list(theme_classic() + theme(text = element_text(family = "Arial", colour = 'black'),
                                              strip.background = element_blank(),
                                              strip.text = element_text(size = 14, face = "bold"),
@@ -95,7 +90,7 @@ theme_custom <- list(theme_classic() + theme(text = element_text(family = "Arial
                                              axis.title = element_text(size = 14, family = "Arial"),
                                              aspect.ratio = 1))
 
-#11A - Observed Features
+#Observed Features (Supplemental Figure 2A)
 observed_features <- read_qza("Outputs/Diversity/unrarefied-observed-features.qza")$data %>%
   rownames_to_column(var='SampleID') %>%
   inner_join(metadata) %>%
@@ -108,7 +103,7 @@ symnum.args <- list(cutpoints = c(0, 0.0001, 0.001, 0.01, 0.05, Inf), symbols = 
 p_observed_features_location <- ggplot(data = observed_features, aes(x = location, y = observed_features)) +
   geom_boxplot() +
   geom_point() +
-  geom_pwc(stat = "pwc", method = "dunn_test",label = "{p.adj.signif}", vjust = 0.6, hide.ns = TRUE, p.adjust.method = "BH", 
+  geom_pwc(stat = "pwc", method = "dunn_test",label = "{p.adj.signif}", vjust = 0.6, hide.ns = TRUE, p.adjust.method = "BH",  #Statistical test for observed features
            label.size = 6, size = 0.5, bracket.nudge.y = .10) +
   labs(y = "Observed ASVs", x = "Sampling Location") +
   theme_custom +
@@ -120,15 +115,8 @@ p_observed_features_location
 
 ggsave("Figures/Diversity/observed_features_location.png", plot = p_observed_features_location, scale =1.5, height = 3, width = 3, unit = "in") 
 
-observed_features %>%
-  group_by(location) %>%
-  get_summary_stats()
 
-kruskal.test(observed_features ~ location, data = observed_features)
-pairwise.wilcox.test(observed_features$observed_features, observed_features$location, p.adjust.method = 'BH')
-
-
-#11B - RA controls
+#Relative abundance of controls (Supplemental Figure 2B)
 taxa_long_genus_control <-
   taxasums %>%
   as.data.frame() %>% #Makes it a dataframe
@@ -138,12 +126,12 @@ taxa_long_genus_control <-
   filter(Abundance != 0) %>% # Filters out 0s to speed things up
   filter(location == 'control') %>% #Deletes the controls
   mutate(location = if_else(grepl('S_Aureus', patient), "Positive Control", "Negative Control")) %>%
-  filter(location == 'Negative Control') %>% #Deletes the Postive controls
+  filter(location == 'Negative Control') %>% #Removes the Postive controls
   group_by(SampleID) %>%
-  mutate(Total_Abundance = sum(Abundance)) %>%
+  mutate(Total_Abundance = sum(Abundance)) %>% #Calculates the total abundance for each sample
   ungroup()  %>%
-  mutate(Relative_Abundance = Abundance/Total_Abundance) %>%
-  mutate(LTC = if_else(Relative_Abundance > 0.05, Taxon, "Other")) %>%
+  mutate(Relative_Abundance = Abundance/Total_Abundance) %>% #Calculates relative abundance
+  mutate(LTC = if_else(Relative_Abundance > 0.05, Taxon, "Other")) %>% #Taxa that have an abundance of less than 5% are designated as other
   separate(Taxon, into = c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus"), sep = ";") %>%
   mutate(LTC = if_else(LTC == "Other", "Other", paste0("g_", str_trim(Genus)))) %>%
   mutate(LTC = if_else(LTC =="g_NA", paste0("f_", str_trim(Family)), LTC))
@@ -157,24 +145,17 @@ relative_abundance_control <- ggplot(taxa_long_genus_control, aes(x = patient,
   scale_fill_manual(values = col) +
   theme_custom +
   theme(axis.text.x = element_blank(), legend.key.size = unit(12, "pt"), legend.text = element_markdown(size = 10), legend.position = "bottom") +
-  #guides(fill = "none")
   guides(fill=guide_legend(title=""))
 relative_abundance_control 
 ggsave("Figures/Relative Abundance/relative_abundnace_control.png", plot = relative_abundance_control, scale =1.5, height = 3, width = 7.5, unit = "in")
 
 
-most_abundant_taxa_genus_control <- 
-  taxa_long_genus_control %>%
-  group_by(SampleID) %>%
-  filter(Relative_Abundance == max(Relative_Abundance))
-
-#11C - Beta diversity
+#Beta Diversity (Supplemental Figure 2C)
 unweighted_unifrac<-read_qza("Outputs/Diversity/unweighted_unifrac_pcoa_results.qza") #Reads inputed PCoA plot using the qiime2r package
 
 unweighted_unifrac_vector <- unweighted_unifrac$data$Vectors %>%
   select("SampleID", PC1, PC2) %>% #Selects the data from the pcoa plot
   left_join(metadata) %>%
-  filter(SampleID != 'TDP_Broncoscope_BAL') %>%
   mutate(location = if_else(location == 'control', if_else(grepl('S_Aureus', patient), "Positive Control", "Negative Control"), location))
 unweighted_unifrac_vector$location <- fct_relevel(unweighted_unifrac_vector$location, "Rectal Swab", "Stool", "Nasal Swab", "Tracheal Swab")
 
@@ -186,12 +167,12 @@ p_beta_diversity <- ggplot(data = unweighted_unifrac_vector, aes(x=PC1, y=PC2, c
   stat_ellipse() + #Creates 95% confidence ellipses around the different categories
   theme_custom +
   theme(legend.key.size = unit(12, "pt"), legend.text = element_markdown(size = 10)) +
-  #guides(color= guide_legend(title=""))
   guides(color = "none")
 p_beta_diversity
 ggsave("Figures/Diversity/unweighted_unifrac.png", plot = p_beta_diversity, scale =1.5, height = 3, width = 3, unit = "in") 
 
-#12 Relative Abundance (Phylum Level)
+#12 Relative abundance phylum level (Supplemental Figure 10)
+
 taxasums_phylum <- summarize_taxa(FeatureTable, taxonomy)$Phylum
 
 taxa_long_phylum <-
@@ -238,7 +219,9 @@ relative_abundance_phylum
 
 ggsave("Figures/Relative Abundance/relative_abundnace_phylum.png", plot = relative_abundance_phylum, scale =1.5, height = 5, width = 9, unit = "in")
 
-# Differential Abundance Load from ANCOMBC script and transform to log2
+# Differential Abundance (Supplemental Figure 11)
+
+# transform to log2
 DA_stool<-read.csv("Outputs/Ancombc2/ancombc2-primary-results-stool.csv") %>%
   mutate(across(lfc_timeD3:lfc_timeD14, function(x) exp(x))) %>%
   mutate(across(lfc_timeD3:lfc_timeD14, function(x) log2(x)))
@@ -251,7 +234,6 @@ DA_nasal<-read.csv("Outputs/Ancombc2/ancombc2-primary-results-nasal.csv") %>%
   mutate(across(lfc_timeD3:lfc_timeD14, function(x) exp(x))) %>%
   mutate(across(lfc_timeD3:lfc_timeD14, function(x) log2(x))) 
 
-#Volcano plots
 DA_stool_to_plot <- DA_stool %>%  
   select(taxon, lfc_timeD3:lfc_timeD14, q_timeD3:q_timeD14) %>% 
   melt("taxon") %>%
